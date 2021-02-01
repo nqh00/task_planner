8291f5d6a5bb93f4fb56f6b867bfc0eff7ffb336
nqhb00 <nqhb00@gmail.com>
2021-02-01 23:43:32 +0700

    Initial commit. Automatically created by Flutter.

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  // This widget is the home page of this application.
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
