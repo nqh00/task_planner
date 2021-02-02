3b1e88cb0ebc0a2092070e8aef0d2d8a85cb3687
nqhb00 <nqhb00@gmail.com>
2021-02-02 22:45:46 +0700

    Display the first task on to the screen by using mapping the list.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './task.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,9 +14,41 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Task> tasks = [
+    Task(title: 'Name of your task', note: 'Detail of your task', check: false),
+  ];
   // This widget is the home page of this application.
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TASK PLANNER'),
+      ),
+      body: SingleChildScrollView(
+          child: Column(children: <Widget>[
+        Card(
+          child: Container(
+            child: Text('CHART', textAlign: TextAlign.center),
+          ),
+        ),
+        Card(
+          child: Column(
+            children: tasks.map((tsk) {
+              return Row(
+                children: <Widget>[
+                  Card(
+                    child: Text('${tsk.check}'),
+                  ),
+                  Column(children: <Widget>[
+                    Text('${tsk.title}'),
+                    Text('${tsk.note}'),
+                  ]),
+                ],
+              );
+            }).toList(),
+          ),
+        ),
+      ])),
+    );
   }
 }
---
+++ lib/task.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart';
+
+class Task {
+  final String title;
+  final String note;
+  final bool check;
+  Task({@required this.title, @required this.note, @required this.check});
+}
